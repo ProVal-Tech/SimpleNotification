c8dfe44cded642ba479ea46b27051181bc3f0377
Stephen <11358399+nixuno@users.noreply.github.com>
2024-12-09 11:30:12 -0500

    Add TOML config option

--- Program.cs
+++ Program.cs
@@ -1,56 +1,88 @@
 ﻿using CommandLine;
 using System;
+using System.IO;
+using System.Runtime.Serialization;
 using System.Windows.Forms;
+using Tomlyn;
 
 namespace SimpleNotification {
     static class Program {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        private static Options clArguments = new Options();
+        
         public class Options {
             [Option(
                 'm',
-                "Message",
+                "message",
                 Required = true,
-                HelpText = "The message to display to the end user."
+                HelpText = "The message to display to the end user.",
+                SetName = "direct"
             )]
             public string Message { get; set; }
 
             [Option(
                 'i',
-                "ImageURL",
+                "imageurl",
                 Required = false,
                 Default = "",
-                HelpText = "The URL for the image to display instead of the default."
+                HelpText = "The URL for the image to display instead of the default.",
+                SetName = "direct"
             )]
-            public string ImageURL { get; set; }
+            public string ImageUrl { get; set; }
 
             [Option(
                 'e',
-                "Email",
+                "email",
                 Required = false,
                 Default = "",
-                HelpText = "The support e-mail address to display to the user."
+                HelpText = "The support e-mail address to display to the user.",
+                SetName = "direct"
             )]
             public string Email { get; set; }
 
             [Option(
                 'p',
-                "Phone",
+                "phone",
                 Required = false,
                 Default = "",
-                HelpText = "The support phone number to display to the user."
+                HelpText = "The support phone number to display to the user.",
+                SetName = "direct"
             )]
             public string Phone { get; set; }
+            
+            [Option(
+                'c',
+                "configfile",
+                Required = true,
+                Default = "",
+                HelpText = "The path to the TOML config file.",
+                SetName = "toml"
+            )]
+            [IgnoreDataMember]
+            public string TomlPath { get; set; }
         }
+
+        private static Options clArguments = new Options();
 
         [STAThread]
         static void Main(string[] args) {
-            CommandLine.Parser.Default.ParseArguments<Options>(args).WithParsed((opts) => clArguments = opts).WithNotParsed((errs) => Environment.Exit(1));
+            Parser parser = new Parser(settings => { settings.CaseSensitive = false; });
+            parser.ParseArguments<Options>(args)
+                .WithParsed((opts) => clArguments = opts)
+                .WithNotParsed((errs) => Environment.Exit(1));
+            if (!string.IsNullOrWhiteSpace(clArguments.TomlPath)) {
+                try {
+                    string toml = File.ReadAllText(clArguments.TomlPath);
+                    clArguments = Toml.ToModel<Options>(toml);
+                } catch {
+                    Console.WriteLine("Error reading TOML file.");
+                    Environment.Exit(1);
+                }
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1(clArguments.Message, clArguments.ImageURL, clArguments.Email, clArguments.Phone));
+            Application.Run(new Form1(clArguments.Message, clArguments.ImageUrl, clArguments.Email, clArguments.Phone));
         }
     }
 
